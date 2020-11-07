fa2fa08abd6ea26cebb030b5a2a08cfaf7820e90
Danny Fajardo <br1ghtch1p@gmail.com>
2020-11-07 22:09:59 +0000

    started second blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,18 +25,21 @@
 // ACTIVE HIGHLIGHT -> navbar.html //
 $(function(){
     var current = location.pathname;
-    $('#mainNav a').each(function(){
+//     $('#mainNav a').each(function(){
+//         var $this = $(this);
+//         if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.addClass('active');
+//             $this.css('background-color', 'white');
+//             $this.css('color', 'black');
+//         }
+//     })
+    $('.mainNav .underline').each(function(){
         var $this = $(this);
-        // if the current path is like this link, make it active
         if($this.attr('href').indexOf(current) !== -1){
             $this.addClass('active');
-            $this.css('background-color', 'white');
-            $this.css('color', 'black');
-        }
+            $this.css('width', '100%');
+            // $this.css('background-color', 'white');
+            $this.css('color', 'white');
     })
-});
-
-
-$(window).load(function(){
-    $('#blogModal').modal('show');
 });
