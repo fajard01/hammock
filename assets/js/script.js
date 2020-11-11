f171212dc1fc043f0c94231be2a4af4b31fc8d8a
Danny Fajardo <br1ghtch1p@gmail.com>
2020-11-11 17:40:46 +0000

    lint scss and restructured

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,37 +9,38 @@
 
 
 // TOGGLE TRANSPARENCY ON SCROLL //
-// $(window).scroll(function(){
-//     var scroll = $(window).scrollTop();
-//     if(scroll > 50){
-//         $('.fixed-top').css('background', 'black');
-//         $('.fixed-top').css('filter', 'invert(0)');
-//     } else{
-//         $('.fixed-top').css('background', 'transparent');
-//         $('.fixed-top').css('filter', 'invert(1)');
-//     }
-// });
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 50) {
+    $('.fixed-top').css('background', '$black');
+    $('.fixed-top').css('filter', 'invert(0)');
+  } else {
+    $('.fixed-top').css('background', 'transparent');
+    $('.fixed-top').css('filter', 'invert(1)');
+  }
+});
+
 // var svg = document.getElementById('svgMap');
 
 
 // ACTIVE HIGHLIGHT -> navbar.html //
-$(function(){
-    var current = location.pathname;
-//     $('#mainNav a').each(function(){
-//         var $this = $(this);
-//         if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.addClass('active');
-//             $this.css('background-color', 'white');
-//             $this.css('color', 'black');
-//         }
-//     })
-    $('.mainNav .underline').each(function(){
-        var $this = $(this);
-        if($this.attr('href').indexOf(current) !== -1){
+$(function () {
+      var current = location.pathname;
+      //     $('#mainNav a').each(function(){
+      //         var $this = $(this);
+      //         if the current path is like this link, make it active
+      //         if($this.attr('href').indexOf(current) !== -1){
+      //             $this.addClass('active');
+      //             $this.css('background-color', 'white');
+      //             $this.css('color', '$black');
+      //         }
+      //     })
+      $('.mainNav .underline').each(function () {
+          var $this = $(this);
+          if ($this.attr('href').indexOf(current) !== -1) {
             $this.addClass('active');
             $this.css('width', '100%');
             // $this.css('background-color', 'white');
             $this.css('color', 'white');
-    })
-});
+          })
+      });
